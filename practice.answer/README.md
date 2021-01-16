32d39012671993033735da6cdab26ff848f103ed
memorykghs <37746042+memorykghs@users.noreply.github.com>
2021-01-17 01:06:13 +0800

    Delete practice.answer directory

--- practice.answer/README.md
+++
@@ -1,7 +0,0 @@
-# Spring-Boot---Notes
-- [ ] 正規表達式
-- [ ] Bean生命週期
-- [ ] Postman圖片截圖 & 講義圖片更換
-- [ ] 8.3. Query with SQL File 圖片
-- [ ] WebExceptionHandler運作以及如何抓到被丟出的Exception
-- [ ] package名稱後墜與功能是否跟Annotation有關係
--- practice.answer/project.answer/src/main/java/com/cathaybk/project/answer/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cathaybk.project.answer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cathaybk.project.answer.entity.CustomerEntity;
-
-public interface CustomerRepository extends JpaRepository<CustomerEntity, Long>{
-
-}
